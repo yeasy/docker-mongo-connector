d2b24e0c116e1fd91c38114c6513360cb58b9303
Baohua Yang <yangbaohua@gmail.com>
2015-08-19 17:04:38 +0800

    Add missing startup.sh

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "rs.isMaster()" > is_master_check
+is_master_result=`mongo --host mongo< is_master_check`
+
+expected_result="\"ismaster\" : true"
+
+while true;
+do
+  if [ "${is_master_result/$expected_result}" = "$is_master_result" ] ; then
+    echo "Waiting for Mongod node to assume primary status..."
+    sleep 3
+    is_master_result=`mongo --host mongo< is_master_check`
+  else
+    echo "Mongod node is now primary"
+    break;
+  fi
+done
+
+mongo-connector -m mongo:27017 -t elasticsearch:9200 -d elastic_doc_manager
